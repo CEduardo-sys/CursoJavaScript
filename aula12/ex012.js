3eb0ea6f50d51400030f03fa49b49968bc94bd79
Eduardo <c-eduardo94@hotmail.com>
2021-08-26 01:52:20 -0300

    Adicionando exercicios do curso em andamento

---
+++ aula12/ex012.js
@@ -0,0 +1,11 @@
+var agora = new Date()
+var hora = agora.getHours()
+var minutos = agora.getMinutes()
+console.log(`Agora são exatamente ${hora}h : ${minutos}min `)
+if(hora < 12) {
+    console.log('Bom Dia!')
+} else if (hora <=18){
+    console.log('Boa Tarde!')
+} else {
+    console.log('Boa Noie!')
+}
